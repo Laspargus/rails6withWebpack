8333cb9bf00694405ac2313ea8e3231d2cea22b9
laspargus <maxime.speroni@gmail.com>
2020-06-01 15:39:25 +0200

    add picture to home and profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  get 'profile/show'
-  resources :users
   root 'home#index'
+  resources :pictures, only: [:create]
+  get 'profile' => 'profile#show'
   devise_for :users
+  resources :users, only: [:show]
+  # get 'users/:id' => 'users#show', as: :user
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
